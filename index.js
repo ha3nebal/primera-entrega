27ce63a109602d31b320fcfdea7df37ad5e7657f
Anibal Allendes <anibalallendesaravena@gmail.com>
2025-12-11 21:03:14 -0300

    Tranformo mi array en ojeto JSON, lo llamo con fetch, agrego librerias

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const pescadosDisponibles = [
-    { id: 1, nombre: "Salmón", precioPorKg: 15000, imagen: "🐟"},
-    { id: 2, nombre: "Albacora", precioPorKg: 12000, imagen: "🦈" },
-    { id: 3, nombre: "Merluza Austral", precioPorKg: 18000, imagen: "🐡" },
-    { id: 4, nombre: "Congrio", precioPorKg: 10000, imagen: "🐋"}
-];
+function cargarpescados(){
+  fetch('pescados.json')
+    .then(response => response.json())
+    .then(data => {
+      imprimirProductosEnHTML(data);
+    })
+    .catch(error => console.error('Error al cargar los pescados:', error));
+}
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 
-function imprimirProductosEnHTML(pescadosDisponibles) {
+function imprimirProductosEnHTML(data) {
   const contenedorPescados = document.getElementById("pescados-container");
+  contenedorPescados.innerHTML = "";
 
-  pescadosDisponibles.forEach((pescados) => {
+  data.forEach((pescados) => {
     let cardPescado = document.createElement("article");
     cardPescado.classList = "pescados-item";
 
@@ -19,8 +22,7 @@ function imprimirProductosEnHTML(pescadosDisponibles) {
         <h2>Pescado: ${pescados.nombre}</h2>
         <p>Precio: $${pescados.precioPorKg}</p>
         <p>Imagen: ${pescados.imagen}</p>
-        <button id="btnComprar${pescados.id}">Comprar</button>
-       
+        <button id="btnComprar${pescados.id}">Comprar</button>   
     `;
 
     contenedorPescados.appendChild(cardPescado);
@@ -28,23 +30,35 @@ function imprimirProductosEnHTML(pescadosDisponibles) {
 
     // Agregar pescados al carrito
     botonComprar.addEventListener("click", () => {
-      alert(`Has comprado: ${pescados.nombre} por $${pescados.precioPorKg}`);
+    const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+    }
+    });
+    Toast.fire({
+    icon: "success",
+    title: "¡Pescado agregado al carrito!"
+   });
 
-      carrito.push({ pescado: pescados.nombre, precio: pescados.precioPorKg });
+      carrito.push({ id: pescados.id, pescado: pescados.nombre, precio: pescados.precioPorKg });
       localStorage.setItem("carrito", JSON.stringify(carrito));
       imprimirCarrito();
     });
   });
 }
 
-
 function finalizarCompra() {
     if (carrito.length === 0) {
-        alert("El carrito está vacío. Agrega productos para finalizar la compra.");
-        return;
+        Swal.fire("El carrito está vacío. Agrega productos antes de finalizar la compra.");
     }
     const total = carrito.reduce((sum, item) => sum + item.precio, 0);
-    alert(`🎉 ¡Compra Finalizada! 🎉 El total de tu compra es: $${total} Gracias por preferirnos.`);
+    Swal.fire(`🎉 ¡Compra Finalizada! 🎉 El total de tu compra es: $${total} Gracias por preferirnos.`);
 
     carrito = [];
     localStorage.removeItem("carrito");
@@ -52,17 +66,48 @@ function finalizarCompra() {
 }
 
 function cancelarCompra() {
+    
     if (carrito.length === 0) {
-        alert("El carrito ya está vacío.");
+         Swal.fire({
+            icon: "info",
+            title: "Carrito vacío",
+            text: "El carrito ya está vacío. No hay nada que cancelar."
+        });
         return;
     }
-    const confirmacion = confirm("¿Estás seguro de que quieres cancelar y vaciar el carrito?");
-    if (confirmacion) {
-        carrito = []; 
-        localStorage.removeItem("carrito");
-        imprimirCarrito();
-        alert("❌ Compra Cancelada. El carrito ha sido vaciado.");
-    }
+
+    Swal.fire({
+        title: "¿Estás seguro?",
+        text: "Esta acción vaciará completamente tu carrito y no se puede deshacer.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminarlo!",
+        cancelButtonText: "No, cancelar!"
+    }).then((result) => {
+        
+        if (result.isConfirmed) {
+            
+            carrito = []; 
+            localStorage.removeItem("carrito");
+            imprimirCarrito(); 
+
+            Swal.fire({
+                title: "Eliminado!",
+                text: "Tu carrito ha sido vaciado",
+                icon: "success"
+            });
+        
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            
+            Swal.fire({
+                title: "Cancelado",
+                text: "Tu carrito y los productos están seguros :)",
+                icon: "error"
+            });
+        }
+    });
 }
 
 function imprimirCarrito() {
@@ -70,13 +115,16 @@ function imprimirCarrito() {
   contenedorCarrito.innerHTML = "<h2>Carrito de Compras</h2>";
   let total = 0;
 
-  carrito.forEach((item, index) => {
-    contenedorCarrito.innerHTML += `<p>${index + 1}. ${item.pescado} - $${item.precio}</p>`;
+  carrito.forEach((item) => {
+    contenedorCarrito.innerHTML += `<article>
+                                    <h3>${item.pescado}</h3> 
+                                    <p>$${item.precio}</p>
+                                    </article>`;
     total += item.precio;
   });
 
   contenedorCarrito.innerHTML += `<h3>Total: $${total}</h3>`;
-}
+};
 
 // Event Listeners de botones finalizar compra, cancelar compra
         document.getElementById("btnFinalizarCompra").addEventListener("click", finalizarCompra);
@@ -84,7 +132,7 @@ function imprimirCarrito() {
 
 
 // Ejecución del programa
-imprimirProductosEnHTML(pescadosDisponibles);
+ cargarpescados();
 
 if (carrito.length > 0) {
   imprimirCarrito();
